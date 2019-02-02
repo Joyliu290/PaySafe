9f0f12ff0a687b2f797f989ca1cd986120316568
Joyliu290 <joyliu290@hotmail.com>
2019-02-02 11:52:52 -0500

    Finished UI for sign-in page

--- app/src/main/java/project/qhacks/com/qhacksfirebaseauth/MainActivity.java
+++ app/src/main/java/project/qhacks/com/qhacksfirebaseauth/MainActivity.java
@@ -2,6 +2,8 @@ package project.qhacks.com.qhacksfirebaseauth;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText enterUserEmail = (EditText) findViewById(R.id.emailEditText);
+        EditText enterUserPassword = (EditText) findViewById(R.id.passwordEditText);
+        Button signInButton = (Button) findViewById(R.id.signInButton);
     }
 }
